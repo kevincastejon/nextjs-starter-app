544bff0f82f7e46e058412ce37a9f57574b77e20
Kevin Castejon <kevincastejon@hotmail.fr>
2019-10-22 02:18:50 +0200

    added next-progressbar & babel-eslint

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
+const withProgressBar = require('next-progressbar');
 
-module.exports = withCSS(
-  withImages({
-    webpack(config) {
-      return config;
-    },
-    exportPathMap() {
-      return {
-        '/': { page: '/' },
-      };
-    },
-  }),
+module.exports = withProgressBar(
+  withCSS(
+    withImages({
+      webpack(config) {
+        return config;
+      },
+      exportPathMap() {
+        return {
+          '/': { page: '/' },
+        };
+      },
+    }),
+  ),
 );
